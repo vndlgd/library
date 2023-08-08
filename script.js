75a451c3a45b83a8d1910952fd2b458f45598bd9
Ivan Delgado <46804151+vndlgd@users.noreply.github.com>
2023-08-08 00:03:09 -0700

    Write function to display each book on the page

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let myLibrary = ["A Game Of Thrones by George R. R. Margin, 694 pages, read", "The Hobbit by J.R.R. Tolkien, 295 pages, not read yet"]
+
 function Book(title, author, pages, read) {
     this.title = title,
         this.author = author,
@@ -9,5 +11,32 @@ function Book(title, author, pages, read) {
     }
 }
 
-// theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", "295", "not read yet");
+// let theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", "295", "not read yet");
 // console.log(theHobbit.info())
+
+function addBookToLibrary() {
+    // take user input
+    let title = prompt("Title: ");
+    let author = prompt("Author: ");
+    let pages = prompt("Pages: ");
+    let read = prompt("Have you read it? (read / not read yet)");
+    let newBook = new Book(title, author, pages, read)
+    // store it into the array
+    myLibrary.push(newBook.info())
+}
+
+// TODO:
+// display each book on the page
+function displayBooks() {
+
+    for (const item of myLibrary) {
+        const book = document.createElement('div');
+        book.classList.add("card")
+        book.textContent += item;
+        display.appendChild(book);
+    }
+}
+
+const display = document.querySelector('#booksDisplay');
+displayBooks();
+
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,2 @@
+let theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", "295", "not read yet");
+console.log(theHobbit.info())

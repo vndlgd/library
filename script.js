46e3f47b27d95e6d570bd1c9a733da70e823f713
Ivan Delgado <46804151+vndlgd@users.noreply.github.com>
2023-08-10 23:03:09 -0700

    Refactor book submission and display logic

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let myLibrary = ["A Game Of Thrones by George R. R. Margin, 694 pages, read", "The Hobbit by J.R.R. Tolkien, 295 pages, not read yet"]
+let myLibrary = []
 
 function Book(title, author, pages, read) {
     this.title = title,
@@ -7,28 +7,12 @@ function Book(title, author, pages, read) {
         this.read = read
 
     this.info = function () {
-        return `${title} by ${author}, ${pages} pages, ${read}`
+        return `${title} by ${author}, ${pages} pages, ${read}`;
     }
 }
 
-// let theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", "295", "not read yet");
-// console.log(theHobbit.info())
-
-function addBookToLibrary() {
-    // take user input
-    let title = prompt("Title: ");
-    let author = prompt("Author: ");
-    let pages = prompt("Pages: ");
-    let read = prompt("Have you read it? (read / not read yet)");
-    let newBook = new Book(title, author, pages, read)
-    // store it into the array
-    myLibrary.push(newBook.info())
-}
-
-// TODO:
-// display each book on the page
 function displayBooks() {
-
+    clearLibrary();
     for (const item of myLibrary) {
         const book = document.createElement('div');
         book.classList.add("card")
@@ -37,6 +21,41 @@ function displayBooks() {
     }
 }
 
-const display = document.querySelector('#booksDisplay');
-displayBooks();
+function clearLibrary() {
+    while (display.lastElementChild) {
+        display.removeChild(display.lastElementChild);
+    }
+}
 
+function openForm() {
+    document.getElementById("myForm").style.display = "block"; // Show the form
+}
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none"; // Hide the form
+}
+
+// main
+const display = document.querySelector('#booksDisplay');
+// Add an event listener to the form outside of submitForm
+let form = document.getElementById("myForm");
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    let title = document.getElementById("title").value;
+    let author = document.getElementById("author").value;
+    let pages = document.getElementById("pages").value;
+    let read = document.getElementById("read").checked;
+
+    let newBook = new Book(title, author, pages, (read) ? 'read' : 'not read yet');
+    myLibrary.push(newBook.info());
+    displayBooks();
+
+    // Clear the form inputs
+    document.getElementById("title").value = "";
+    document.getElementById("author").value = "";
+    document.getElementById("pages").value = "";
+    document.getElementById("read").checked = false;
+
+    closeForm();
+});

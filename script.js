ad3bc423ce82cef08812b865371c813e547bf994
Ivan Delgado <46804151+vndlgd@users.noreply.github.com>
2023-08-18 14:37:56 -0700

    Add button on each book's display to change its read status

--- script.js
+++ script.js
@@ -1,22 +1,60 @@
-let myLibrary = []
+let myLibrary = [];
 
 function Book(title, author, pages, read) {
-    this.title = title,
-        this.author = author,
-        this.pages = pages,
-        this.read = read
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
 
     this.info = function () {
-        return `${title} by ${author}, ${pages} pages, ${read}`;
-    }
+        return `${title}\n${author}\n${pages} pages\n${read}`;
+    };
 }
 
 function displayBooks() {
     clearLibrary();
-    for (const item of myLibrary) {
+    // by index to be able to remove cards
+    for (let i = 0; i < myLibrary.length; i++) {
         const book = document.createElement('div');
-        book.classList.add("card")
-        book.textContent += item;
+        const remove = document.createElement('button');
+        const read = document.createElement('button');
+
+        book.classList.add("card");
+        book.textContent = myLibrary[i];
+
+        const bookInfo = myLibrary[i].split('\n');
+        const title = bookInfo[0];
+        const author = bookInfo[1];
+        const pages = bookInfo[2];
+        const readStatus = bookInfo[3] === 'true';
+
+        book.innerHTML = `${title}<br>${author}<br>${pages}`;
+
+        remove.textContent = 'Remove';
+        remove.classList.add("removeButton");
+
+        read.classList.add("readButton");
+
+        if (readStatus) {
+            read.style.backgroundColor = "var(--light-green)";
+            read.textContent = "Read";
+        } else {
+            read.style.backgroundColor = "var(--light-red)";
+            read.textContent = "Not read";
+        }
+
+        remove.addEventListener('click', () => {
+            // remove card by index
+            removeBook(i);
+        });
+
+        read.addEventListener('click', () => {
+            myLibrary[i] = toggleReadStatus(bookInfo);
+            displayBooks(); // Refresh the display to update the button text
+        });
+
+        book.appendChild(read);
+        book.appendChild(remove);
         display.appendChild(book);
     }
 }
@@ -27,6 +65,11 @@ function clearLibrary() {
     }
 }
 
+function removeBook(index) {
+    myLibrary.splice(index, 1);
+    displayBooks();
+}
+
 function openForm() {
     document.getElementById("myForm").style.display = "block"; // Show the form
 }
@@ -35,7 +78,23 @@ function closeForm() {
     document.getElementById("myForm").style.display = "none"; // Hide the form
 }
 
+function toggleReadStatus(bookInfo) {
+    const title = bookInfo[0];
+    const author = bookInfo[1];
+    const pages = bookInfo[2];
+    const readStatus = bookInfo[3] === 'true';
+
+    if (readStatus) {
+        bookInfo[3] = 'false';
+    } else {
+        bookInfo[3] = 'true';
+    }
+
+    return bookInfo.join('\n'); // Return the updated book string
+}
+
 // main
+let index = 0;
 const display = document.querySelector('#booksDisplay');
 // Add an event listener to the form outside of submitForm
 let form = document.getElementById("myForm");
@@ -47,7 +106,7 @@ form.addEventListener("submit", (e) => {
     let pages = document.getElementById("pages").value;
     let read = document.getElementById("read").checked;
 
-    let newBook = new Book(title, author, pages, (read) ? 'read' : 'not read yet');
+    let newBook = new Book(title, author, pages, read);
     myLibrary.push(newBook.info());
     displayBooks();
 
